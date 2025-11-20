56af228c241248cfc11ad14ebc34ce2feed00973
Your Name <youremail@example.com>
2025-11-20 17:34:53 +0530

    01-folder

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,35 @@
+import ReactJS from 'react'
+
+const App = () => {
+  return (
+    <div className='parent'>
+<div className="card">
+  <div className="top">
+    <button>Save</button>
+    <img src="https://thumbs.dreamstime.com/b/amazon-logo-white-background-montreal-canada-july-printed-paper-98221126.jpg" alt="" />
+    
+  </div>
+  <div className="center"></div>
+  <h3>Amazon <span>5 DAys ago</span></h3>
+  <h2> Senior UI/UX Designer</h2>
+  <div>
+    <h4>Part Time</h4>
+    <h4> Senior Level</h4>
+  </div>
+
+</div>
+<div className="bottom">
+  <div>
+    <div>
+      <h3>$120/hr</h3>
+      <p>Mumbai,India</p>
+    </div>
+    <button>Apply Now</button>
+  </div>
+
+
+</div>
+    </div>
+  )
+}
+export default App
